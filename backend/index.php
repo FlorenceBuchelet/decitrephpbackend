479aef32c2728def03203d64cb470247fe50bc7c
Florence Buchelet <florence.buchelet@gmail.com>
2024-03-22 09:51:58 +0100

    routing

--- backend/index.php
+++ backend/index.php
@@ -1,29 +1,11 @@
 <?php
 
 // FIXME: update route
-require __DIR__ . './vendor/autoload.php';
-require('./src/Models/userModel.php');
+require './services/databaseConnect.php';
+require './src/Models/userModel.php';
 
-// FIXME: update dotenv route
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../');
-$dotenv->load();
-
-// Comment this for backend tests
-$allowed_origin = 'http://localhost:5173';
-header("Access-Control-Allow-Origin: $allowed_origin");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Specificity, Authorization");
-header("Access-Control-Allow-Credentials: true");
-//
-
-function dbConnect() {
-    try {
-        $dbh = new \PDO($_ENV['DSN'], $_ENV['USER']);
-        return $dbh;
-    } catch (\PDOException $exception) {
-        return  'Error in connection' . $exception->getMessage();
-    }
-}
+$dbh = dbConnect();
 
 
 $URI = getOneUser();
+
